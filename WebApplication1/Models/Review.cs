5ee5f375000e416368141105a63912ba39061ee2
kPlusPlus <kresimir.ivkovic@gmail.com>
2017-12-03 20:56:15 +0100

    d
    d

--- WebApplication1/Models/Review.cs
+++ WebApplication1/Models/Review.cs
@@ -3,15 +3,11 @@
     public class Review
     {
         public int ReviewID{ get; set; }
-        public int AlbumID { get; set; }
-        
+        public int AlbumID { get; set; }        
         public virtual Album Album { get; set; }
 
-        public string Contents { get; set; }
-
-        
+        public string Contents { get; set; }        
         public string ReviewerEmail { get; set; }
-
 
     }
 }
